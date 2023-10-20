1fedcfd59376052762db8854a720ebe6f572bfe2
Tobias Arens <t.arens@fz-juelich.de>
2023-10-20 10:51:34 +0900

    fix formatting of parameters

--- bleak/network.py
+++ bleak/network.py
@@ -17,10 +17,15 @@ class Upstream:
         # return value is "DeviceID,Time,Close count,Total count,Avg RSSI,Std RSSI,Min RSSI,Max RSSI"
         summary = prepare_row_data_summary(id, timestamp, rssi_list, close_threshold)
 
-        date = datetime.now().strftime("%Y%M%D")
+        # {'device_id': '45', 'date': '20231020', 'time': '104000', 'count': '26', 'total': '26', 'rssi_avg': '-93.615', 'rssi_std': '3.329', 'rssi_min': '-99', 'rssi_max': '-85'}
+        
+        # %Y%m%d,%H%M%S
+        date = datetime.now().strftime("%Y%m%d")
 
-        params = {'device_id':id,'date':date,'time':timestamp,'count':summary[2],'total':summary[3],
+        params = {'device_id':id,'date':date,'time':timestamp.replace(':', ''),'count':summary[2],'total':summary[3],
                                     'rssi_avg':summary[4],'rssi_std':summary[5],'rssi_min':summary[6],'rssi_max':summary[7]}
+
+        #print(f"sending request with params: {params}")
 
         async with aiohttp.ClientSession() as session:
             async with session.get(self.url, params=params) as resp:
