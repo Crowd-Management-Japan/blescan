6c27439239fe2bdfea65f65f27eaa49e984fc8c1
Claudio Feliciani <claudio.feliciani@gmail.com>
2025-07-30 18:01:28 +0900

    added logging command (minor)

--- blescan/network.py
+++ blescan/network.py
@@ -227,6 +227,9 @@ class InternetStorage:
             self.com.enqueue_count_message(params)
 
     def save_transit(self, id: int, timestamp: str, close_ble_list: list):
+
+        logger.debug(f"data: {close_ble_list}")
+        
         if Config.Transit.use_internet:
             params = {
                 'id':id,
